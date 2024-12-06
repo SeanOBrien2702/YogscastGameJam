6836cd37c996ae1d22a95963f664321a3f10f649
SeanOBrien2702 <seanobrien2702@gmail.com>
2024-12-06 10:22:50 -0500

    Added dialogue controller

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.U2D.Animation;
@@ -7,12 +8,14 @@ public enum NPC
     Kid, Lady, Mayor, Nerd, Man, Woman
 }
 
-public class NPCController : MonoBehaviour
+public class NPCController : MonoBehaviour, IInteractable
 {
+    public static event Action<string[]> OnNPCSelected = delegate { };
     SpriteRenderer spriteRenderer;
     SpriteLibrary library;
     [SerializeField] NPC npc = NPC.Kid;
     int[] sequence = new int[] { 0, 1, 2, 1, 3, 1, 2, 1};
+    [SerializeField] string[] dialogue;
 
     void Start()
     {
@@ -25,16 +28,19 @@ public class NPCController : MonoBehaviour
     {
         int index = 0;
         while (true)
-        {
-            Debug.Log(index);
+        {           
             spriteRenderer.sprite = library.GetSprite(npc.ToString(), sequence[index].ToString());
             index++;
             if(index >= sequence.Length)
             {
                 index = 0;
             }
-
             yield return new WaitForSeconds(0.15f);
         }
+    }
+
+    public void Interact()
+    {
+        OnNPCSelected?.Invoke(dialogue);
     }
 }
